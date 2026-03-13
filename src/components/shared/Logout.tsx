71129144cebc6fcd038e7e695c4573b04164da0c
Youssef Turkey <youssef.turkey11@gmail.com>
2026-03-13 17:24:59 +0200

    sidebar responsive

--- src/components/shared/Logout.tsx
+++ src/components/shared/Logout.tsx
@@ -2,10 +2,20 @@
 import { removeAuthCookie } from "@/utils/cookies";
 import { Button } from "../ui/button";
 import { LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 const Logout = ({ collapsed }: { collapsed: boolean }) => {
+  const router = useRouter();
   return (
-    <Button className={"w-full py-5"} onClick={removeAuthCookie}>
+    <Button
+      className={"w-full py-5"}
+      onClick={() => {
+        removeAuthCookie();
+        toast.success("Logout Successfully");
+        router.push("/login");
+      }}
+    >
       {collapsed ? (
         <LogOut size={30} />
       ) : (
